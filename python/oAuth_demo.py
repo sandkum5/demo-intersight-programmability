357c4e5a005bb908e0213009e0844a1408af9078
Sandeep Kumar <58764439+sandkum5@users.noreply.github.com>
2024-02-29 23:29:34 -0800

    Update oAuth_demo.py

--- python/oAuth_demo.py
+++ python/oAuth_demo.py
@@ -3,24 +3,29 @@
     Intersight API calls using oAuth
 """
 
+import sys
 import requests
 from pprint import pprint
 
 def get_token(client_id, client_secret):
     """ Get oAuth Token """
-    token_url='https://intersight.com/iam/token'
+    token_url="https://intersight.com/iam/token"
     client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
     post_data = {"grant_type": "client_credentials"}
     response = requests.post(url=token_url,
                             auth=client_auth,
                             data=post_data)
+    if response.status_code != 200:
+        print("Failed to obtain token from the OAuth 2.0 server", file=sys.stderr)
+        sys.exit(1)
+    print("Successfuly obtained a new token")
     token_json = response.json()
     return token_json["access_token"]
 
 def get_org(token, client_id, client_secret):
     """ Get Organization Info """
     api_url = "https://intersight.com/api/v1/organization/Organizations?$filter=Name eq 'default'"
-    headers = {'Authorization': 'Bearer ' + token}
+    headers = {"Authorization": f"Bearer {token}"}
     response = requests.get(url=api_url, headers=headers)
     if	response.status_code == 401:
         token = get_token(client_id, client_secret)
@@ -28,7 +33,7 @@ def get_org(token, client_id, client_secret):
     else:
         pprint(response.json())
 
-def main():
+if __name__ == '__main__':
     # Set variables
     client_id = "Add Client ID"    # Update
     client_secret = "Add Client Secret" # Update
@@ -38,6 +43,3 @@ def main():
 
     # Get Organization Info
     get_org(token, client_id, client_secret)
-
-if __name__ == '__main__':
-    main()
